5f2e62da11da0e2a3d533f46b6bd2e5aee24b439
Dima Rekesh <bmwshop@yahoo.com>
2016-11-16 15:59:55 -0800

    created digits.sh
    installation script for digits

---
+++ week10/labs-nn/digits.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+# this installs NVIDIA DIGITS in a fresh VM using docker.
+
+# install docker
+apt-get update
+apt-get install -y curl
+cd /tmp
+
+# we assume here that the drivers are installed. if not, uncomment..
+# curl http://AFED.http.sjc01.cdn.softlayer.net/nvidia/NVIDIA-Linux-x86_64-370.28.run -o /tmp/NVIDIA-Linux-x86_64-370.28.run
+# chmod a+x ./NVIDIA-Linux-x86_64-370.28.run
+# ./NVIDIA-Linux-x86_64-370.28.run -a --update
+
+curl http://AFED.http.sjc01.cdn.softlayer.net/nvidia/nvidia-docker_1.0.0.rc.3-1_amd64.deb -o /tmp/nvidia-docker_1.0.0.rc.3-1_amd64.deb
+
+# the drivers
+chmod a+x ./NVIDIA-Linux-x86_64-370.28.run
+
+# docker
+apt-get install -y apt-transport-https ca-certificates
+apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main"  | sudo tee /etc/apt/sources.list.d/docker.list
+apt-get update
+apt-get install -y linux-image-extra-$(uname -r) linux-image-extra-virtual
+apt-get update
+apt-get install -y docker-engine
+
+# nvidia docker
+dpkg -i nvidia-docker_1.0.0.rc.3-1_amd64.deb
+
+
+service docker start
+
+# the data dir is shared with digits
+mkdir -m 777 /data
+
+# it may fail the first time because nvidia!
+nvidia-docker run --name digits -v /data:/data -d -p 5000:34448 nvidia/digits
+
+nvidia-docker run --name digits -v /data:/data -d -p 5000:34448 nvidia/digits
